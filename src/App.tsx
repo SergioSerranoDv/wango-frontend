d64a55b08231e89df4a8fac0f494b1ef83b40117
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-03-11 08:59:56 -0500

    fix: refactor App component to include authentication and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          {isAuthenticated ? (
+            <Route
+              path="/"
+              element={
+                <>
+                  <div className="App">
+                    <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                      </p>
+                      <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Learn React
+                      </a>
+                    </header>
+                  </div>
+                </>
+              }
+            />
+          ) : (
+            <Route path="/" element={<LoginPage />} />
+          )}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const LoginPage = () => {
+  const { loginWithRedirect } = useAuth0();
+  loginWithRedirect();
+  return <div>Login</div>;
+};
 
 export default App;
