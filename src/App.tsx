669553f6888d52117a9ac0f3dc0963c79435d16b
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-03-13 12:16:45 -0500

    fix: Refactor imports and remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ApiContextProvider } from "./context/ApiContext";
 import { Dahsboard } from "./pages/Dahsboard";
-import { MyProfile } from "../pages/MyProfile";
-import "./App.css";
 import MyProfile from "./pages/MyProfile";
+import "./App.css";
 
 function App() {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -33,7 +32,6 @@ function App() {
             <Route path="/" element={<LoginPage />} />
           </>
         )}
-
       </Routes>
     </BrowserRouter>
   );
