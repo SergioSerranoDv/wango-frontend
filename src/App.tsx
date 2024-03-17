5753617f2f15ba5195f786c16952141742adc576
FernandoSilva02 <u20201186317@usco.edu.co>
2024-03-17 14:22:26 -0500

    fix: se trackeo las rutas de addlote y del lotemenu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ApiContextProvider } from "./context/ApiContext";
-import { Dahsboard } from "./pages/Dahsboard";
+import Dahsboard from "./pages/Dahsboard";
+import DashboardLotes from "./pages/DashboardLotes";
 import MyProfile from "./pages/MyProfile";
-import MainMenu from "./components/mainMenu";
-import "./styles/components/mainMenu";
+import LoteForm from "./pages/AddLote";
+import MainMenu from "./components/MainMenu";
+import "./styles/MainMenuStyles";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -29,6 +30,8 @@ function App() {
               }
             ></Route>
             <Route path="/myProfile" element={<MyProfile />} />
+            <Route path="/addLote" element={<LoteForm />} />
+            <Route path="/loteMenu" element={<DashboardLotes />} />
           </>
         ) : (
           <>
