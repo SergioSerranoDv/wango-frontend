68f2f7d45acb34309383cd485f201de0388cbb45
JuanPaGiron <jpgm803@gmail.com>
2024-03-21 21:51:16 -0500

    Changes

---
+++ src/pages/NewCrop.tsx
@@ -0,0 +1,104 @@
+import Navbar from "../components/Navbar";
+import {
+  Button,
+  Description,
+  DetailsItem,
+  DetailsSign,
+  Form,
+  InfoContainer,
+  Input,
+  Label,
+  RegisterFormContainer,
+  SignBoard,
+} from "../styles/FormStyles";
+import React, { useState } from "react";
+
+export default function NewCrop() {
+  const [formData, setFormData] = useState({
+    cropName: "",
+    area: "",
+    latitude: "",
+    longitude: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Formulario enviado:", formData);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <RegisterFormContainer>
+        <SignBoard $custom1>Agrega un nuevo cultivo al lote ‘MisPruebas01’</SignBoard>
+        <InfoContainer>
+          <DetailsSign>
+            ID lote: <DetailsItem>24 Example</DetailsItem>
+          </DetailsSign>
+          <DetailsSign>
+            Área disponible: <DetailsItem>2,7 Example Ha</DetailsItem>
+          </DetailsSign>
+          <DetailsSign>
+            Área en ocupación: <DetailsItem>17,3 Example Ha</DetailsItem>
+          </DetailsSign>
+        </InfoContainer>
+        <Form onSubmit={handleSubmit}>
+          <Label htmlFor="cropName">Nombre del cultivo</Label>
+          <Input
+            type="text"
+            id="cropName"
+            name="cropName"
+            value={formData.cropName}
+            onChange={handleChange}
+            required
+          />
+
+          <Label htmlFor="area">Área</Label>
+          <Input
+            type="number"
+            id="area"
+            name="area"
+            value={formData.area}
+            onChange={handleChange}
+            required
+          />
+
+          <Label htmlFor="latitude">Latitud</Label>
+          <Input
+            type="number"
+            id="latitude"
+            name="latitude"
+            value={formData.latitude}
+            onChange={handleChange}
+            required
+          />
+
+          <Label htmlFor="longitude">Longitud</Label>
+          <Input
+            type="number"
+            id="longitude"
+            name="longitude"
+            value={formData.longitude}
+            onChange={handleChange}
+            required
+          />
+          <Button type="submit" $custom1>
+            Añadir cultivo
+          </Button>
+
+          <Description $custom1>
+            Podrás añadir un encargado a este cultivo en la sección de ‘Mis cultivos’ en el menú
+            principal.
+          </Description>
+        </Form>
+      </RegisterFormContainer>
+    </div>
+  );
+}
--- src/styles/FormStyles.tsx
+++ src/styles/FormStyles.tsx
@@ -12,10 +12,11 @@ export const RegisterFormContainer = styled.div`
   margin: 0 auto;
   background-color: #ffffff;
   padding: 20px;
-  display: flex; /* Utiliza flexbox para centrar verticalmente */
+  display: flex;
   flex-direction: column;
-  justify-content: center; /* Centrar verticalmente */
-  height: 100vh; /* Establece la altura al 100% del viewport para centrar en toda la pantalla */
+  justify-content: center;
+  height: 100vh;
+  position: relative; /* Añade posición relativa */
 `;
 
 export const Form = styled.form`
@@ -36,7 +37,7 @@ export const Label = styled.label<{ $primary?: boolean }>`
 `;
 
 export const Input = styled.input<InputProps>`
-  margin: ${(props) => (props.$primary ? "-12px 0 8px 0" : "6px 0 4px 0")};
+  margin: ${(props) => (props.$primary ? "-12px 0 12px 0" : "6px 0 4px 0")};
   padding: 9.5px;
   font-size: 14px;
   color: #4d4d4d;
@@ -49,7 +50,7 @@ export const Input = styled.input<InputProps>`
   //background-color: #ff0078;
 `;
 
-export const Button = styled.button`
+export const Button = styled.button<InputProps>`
   font-size: 14px;
   font-weight: 550; /*semibold*/
   padding: 10px;
@@ -58,28 +59,41 @@ export const Button = styled.button`
   cursor: pointer;
   border-radius: 6px;
   border: 1px #ff670f;
+  margin-bottom: ${(props) => (props.$custom1 ? "-10px" : "")};
+  margin-bottom: ${(props) => (props.$primary ? "10px" : "")};
+  margin-top: ${(props) => (props.$custom1 ? "10px" : "")};
 `;
 
 export const Logo = styled.img`
   max-width: 277px;
-  max-height: 90px;
+  max-heigh: 90px;
   align-self: center;
   margin-bottom: -22px; /* Espacio entre el logo Wango y el formulario */
 `;
 
 export const SignBoard = styled.p<InputProps>`
-  font-size: ${(props) => (props.$primary ? "13px" : "14px")};
+  font-size: ${(props) => (props.$primary ? "13px" : "13px")};
   font-weight: ${(props) => (props.$primary ? "" : "620")};
   text-align: center;
   color: ${(props) => (props.$primary ? "#000000" : "#4d4d4d")};
-  margin: ${(props) => (props.$primary ? "20px 0 20px 0" : "0 0 40px 0")};
+  margin: ${(props) => (props.$primary ? "20px 0 20px 0" : "0 0 24px 0")};
+  margin-top: ${(props) => (props.$custom1 ? "-250px" : "10px")};
+  margin-bottom: ${(props) => (props.$custom1 ? "40px" : "10px")};
+  margin-top: ${(props) => (props.$custom2 ? "10px" : "")};
+  margin-bottom: ${(props) => (props.$custom2 ? "42px" : "")};
+  margin: ${(props) => (props.$custom ? "-24px 0px 35px 0px" : "")};
+  font-weight: ${(props) => (props.$custom ? "450" : "")};
+  color: ${(props) => (props.$custom ? "#D80000" : "")};
   opacity: 0.95;
 `;
 
 export const Description = styled.p<InputProps>`
-  font-size: 12px;
+  font-size: ${(props) => (props.$custom1 ? "13px" : "12px")};
+  text-align: ${(props) => (props.$custom1 ? "center" : "")};
   //color: ${(props) => (props.$primary ? "#a0cac5" : "#a0cac5")};
-  padding: 0px;
+  //background: ${(props) => (props.$custom1 ? "#bb9de4" : "#bb9de4")};
+  border-bottom: ${(props) => (props.$custom1 ? "10px solid #000000" : "")};
+  margin-top: "-20px";
   color: #4d4d4d;
   opacity: 0.95;
   border: 0px;
@@ -114,4 +128,26 @@ export const FormField = styled.div<InputProps>``;
 export const Link = styled.a<InputProps>`
   color: ${(props) => (props.$primary ? "#548af7" : "#4d4d4d")};
   cursor: pointer;
+`;
+
+export const InfoContainer = styled.div<InputProps>`
+  display: flex;
+  flex-direction: column;
+  gap: 7.5px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #000000;
+  padding-bottom: 18px;
+  margin-bottom: 26px;
+  margin-top: -6px;
+`;
+
+export const DetailsSign = styled.a<InputProps>`
+  font-size: 13px;
+  font-weight: bold;
+  color: #3dac17;
+`;
+
+export const DetailsItem = styled.a<InputProps>`
+  color: #000000;
+  font-weight: 300;
 `;
