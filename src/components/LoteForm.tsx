32c5c38d5813645222e4c7b7119c22c27f991c70
FernandoSilva02 <u20201186317@usco.edu.co>
2024-03-21 22:36:28 -0500

    feat: LoteForm

--- src/components/LoteForm.tsx
+++ src/components/LoteForm.tsx
@@ -1,3 +1,6 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+
 import {
   FormWrapper,
   Form,
@@ -9,23 +12,51 @@ import {
   Button,
 } from "../styles/AddLoteStyles";
 
+interface FormData {
+  nombreLote: string;
+  capacidadLote: string;
+}
+
 function LoteForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Lógica para guardar los cambios del formulario
+  const [formData, setFormData] = useState<FormData>({
+    nombreLote: '',
+    capacidadLote: ''
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3230/api/lotes/', formData);
+      console.log('Respuesta del servidor:', response.data);
+      setFormData({
+        nombreLote: '',
+        capacidadLote: ''
+      });
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
+    }
+  };
+  
 
   return (
     <FormWrapper>
       <Form onSubmit={handleSubmit}>
         <FormHeader>Crea un nuevo lote, ingresa los datos</FormHeader>
         <FormField>
-          <Label htmlFor="name">Nombre del lote</Label>
-          <Input id="name" type="text" required />
+          <Label htmlFor="nombreLote">Nombre del lote</Label>
+          <Input id="nombreLote" name="nombreLote" type="text" value={formData.nombreLote} onChange={handleChange} required />
         </FormField>
         <FormField>
-          <Label htmlFor="capacity">Capacidad</Label>
-          <Input id="capacity" type="text" required />
+          <Label htmlFor="capacidadLote">Capacidad</Label>
+          <Input id="capacidadLote" name="capacidadLote" type="text" value={formData.capacidadLote} onChange={handleChange} required />
         </FormField>
         <ButtonContainer>
           <Button type="submit">Añadir Lote</Button>
