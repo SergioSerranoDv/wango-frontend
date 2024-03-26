c0af149356be15c1722a21d29dee78ab5ea2c46c
FernandoSilva02 <u20201186317@usco.edu.co>
2024-03-26 15:56:46 -0500

    fix: Navbar funcionando y organizado como lo hizo Sergio

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,73 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 import {
   LeftContainer,
   NavbarContainer,
   NavbarInnerContainer,
-  NavbarLink,
   NavbarLinkContainer,
   RightContainer,
   Logo,
+  Text,
 } from "../styles/NavbarStyles";
 import Dropdown from "../components/Dropdown";
 import LogoImg from "../assets/images/logo_navbar.svg";
 
-function Navbar() {
+interface NavbarProps {
+  id: number;
+  elementList: JSX.Element;
+}
+interface LinkElementProps {
+  text: string;
+  link: string;
+}
+
+const Navbar: React.FC = () => {
+  const { logout } = useAuth0();
+  const NavbarItems: NavbarProps[] = [
+    {
+      id: 1,
+      elementList: <LinkElement text="Editar perfil" link="/MyProfile" />,
+    },
+    {
+      id: 2,
+      elementList: <LinkElement text="Configurar variables" link="/variables" />,
+    },
+    {
+      id: 3,
+      elementList: (
+        <button
+          style={{
+            border: "none",
+            background: "none",
+            cursor: "pointer",
+          }}
+          onClick={() => logout()}
+        >
+          <Text>Cerrar sesión</Text>
+        </button>
+      ),
+    },
+  ];
+
   return (
     <NavbarContainer>
       <NavbarInnerContainer>
         <LeftContainer>
           <NavbarLinkContainer>
-            <NavbarLink to="/myProfile">Editar perfil</NavbarLink>
-            <NavbarLink to="/variables">Configurar variables</NavbarLink>
-            <NavbarLink to="/logout">Cerrar sesión</NavbarLink>
             <Dropdown></Dropdown>
+            {NavbarItems.map((item, index) => (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                key={index}
+              >
+                {item.elementList}
+              </div>
+            ))}
           </NavbarLinkContainer>
         </LeftContainer>
         <RightContainer>
@@ -31,6 +78,12 @@ function Navbar() {
       </NavbarInnerContainer>
     </NavbarContainer>
   );
-}
+};
+
+const LinkElement: React.FC<LinkElementProps> = ({ text, link }) => (
+  <Link to={link} style={{ textDecoration: "none", color: "inherit" }}>
+    <Text>{text}</Text>
+  </Link>
+);
 
 export default Navbar;
