eed3821ebd9d92973c0ae162f30e975806682329
FernandoSilva02 <u20201186317@usco.edu.co>
2024-04-02 18:42:40 -0500

    Lotes

--- src/pages/EditLote.tsx
+++ src/pages/EditLote.tsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import LoteFormEdit from "../components/LoteFormEdit";
 
 export default function EditLote() {
-  const { id } = useParams(); // Obtener el ID del lote de la URL
-
-  // Supongamos que obtienes el userId y el lotId de algún lugar
-  const userId = "google-oauth2|116221184137793501360"; // Reemplaza con el ID del usuario actual
-  const lotId = id; // Utilizamos el ID del lote obtenido de la URL
+  const { id } = useParams<{ id?: string }>(); // Indica que 'id' podría ser 'undefined'
 
   return (
     <div>
       <Navbar />
-      <LoteFormEdit lotId={lotId} userId={userId} />
+      <LoteFormEdit />{/* Verifica si 'id' no es 'undefined' antes de pasarla al componente */}
     </div>
   );
 }
