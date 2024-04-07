28684adfc9d0f00f9996d7b0d72a70c458972935
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-04-07 18:28:08 -0500

    Refactor AppContextProvider to use serviceIsReady instead of tokenIsReady in AppContext.tsx

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -53,7 +53,7 @@ export const AppContext = createContext<ContextData>({
 });
 
 export const AppContextProvider: React.FC<PropsWithChildren> = (props) => {
-  const { backendApiCall, tokenIsReady, userToken } = useContext(ApiContext);
+  const { backendApiCall, serviceIsReady } = useContext(ApiContext);
   const [refetchData, setRefetchData] = useState<boolean>(false);
   const [appContextIsFetching, setAppContextIsFetching] = useState<boolean>(true);
   const [userData, setUserData] = useState(UserDataInit);
@@ -68,10 +68,10 @@ export const AppContextProvider: React.FC<PropsWithChildren> = (props) => {
         setAppContextIsFetching(false);
       }
     };
-    if (tokenIsReady) {
+    if (serviceIsReady) {
       getUserData();
     }
-  }, [tokenIsReady, refetchData]);
+  }, [serviceIsReady, refetchData]);
 
   return (
     <AppContext.Provider value={{ userData, appContextIsFetching, refetchData, setRefetchData }}>
