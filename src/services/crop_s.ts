11ea82786060becb50f10197cca32db3c8bd2c66
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-04-08 21:33:00 -0500

    Add fetchPaginatedCropsByLotId function to crop_s.ts

--- src/services/crop_s.ts
+++ src/services/crop_s.ts
@@ -6,3 +6,12 @@ export const createNewCrop = async (
 ) => {
   return await backendApiCall({ method: "POST", endpoint: "v1/crop/new", body: data });
 };
+export const fetchPaginatedCropsByLotId = async (
+  backendApiCall: (data: ApiProps) => Promise<apiResponse>,
+  { page, limit, lotId }: { page: number; limit: number; lotId: string }
+): Promise<any> => {
+  return await backendApiCall({
+    method: "GET",
+    endpoint: `v1/crop/paginated?page=${page}&limit=${limit}&lot_id=${lotId}`,
+  });
+};
