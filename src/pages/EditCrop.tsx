0e186d18688235a431ab4089b09f2c6439558576
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-04-12 22:37:59 -0500

    Refactor EditCrop component to remove unused import and commented code

--- src/pages/EditCrop.tsx
+++ src/pages/EditCrop.tsx
@@ -2,15 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import LoteFormEdit from "../components/LoteFormEdit";
-import CropFormEdit from "../components/CropFormEdit";
 
 export default function EditCrop() {
   const { id } = useParams();
   const cropId = id;
 
-  return (
-    <div>
-      <CropFormEdit cropId={cropId} />
-    </div>
-  );
+  return <div>{/* <CropFormEdit cropId={cropId} /> */}</div>;
 }
