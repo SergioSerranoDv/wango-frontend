49a269164ba727c6aa40aa75e3f2d7b50f7926d4
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-04-25 09:23:37 -0500

    fix: sort staments

--- src/interfaces/notification.ts
+++ src/interfaces/notification.ts
@@ -1,15 +1,12 @@
-import { redirect } from "react-router-dom";
-
-export const NotificationDataInit = {
-  title: "",
-  description: "",
-  status: "",
-  redirectUrl: "",
-};
-
 export interface NotificationI {
   title: string;
   description: string;
   status: string;
   redirectUrl: string;
 }
+export const NotificationDataInit = {
+  title: "",
+  description: "",
+  status: "",
+  redirectUrl: "",
+};
