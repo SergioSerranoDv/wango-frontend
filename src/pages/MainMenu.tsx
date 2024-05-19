de210382b4f63c421bb95d5d20ab321768751369
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2024-05-19 17:02:02 -0500

    refactor: Remove unused code and update MainMenu layout

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,106 +1,59 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { Link } from "react-router-dom";
+import { Menu, ItemsMenu, Item, Text, Description } from "../styles/HomeStyles";
 
-import { MainWrapper, ContentArea, Menu, ItemsMenu, Item, Text } from "../styles/MainMenuStyles";
-import Navbar from "../components/Navbar";
+import { MainLayout } from "../layouts/MainLayout";
 
 import MisLotes from "../assets/icons/myBatches.svg";
 import MiPerfil from "../assets/icons/myProfile.svg";
-import CrearTrabajador from "../assets/icons/createWorkerUser.svg";
-import VerTrabajador from "../assets/icons/viewMyWorkers.svg";
-import CerrarSesion from "../assets/icons/logoutIcon.svg";
-import { useAuth0 } from "@auth0/auth0-react";
+// import CrearTrabajador from "../assets/icons/createWorkerUser.svg";
+// import VerTrabajador from "../assets/icons/viewMyWorkers.svg";
+// import CerrarSesion from "../assets/icons/logoutIcon.svg";
 
-interface MenuProps {
-  id: number;
-  elementList: JSX.Element;
-}
 interface LinkElementProps {
   src: string;
   text: string;
   link: string;
   alt?: string;
 }
-const MainMenu: React.FC = () => {
-  const { userData } = useContext(AppContext);
-  const { logout } = useAuth0();
-  const MenuItems: MenuProps[] = [
-    {
-      id: 1,
-      elementList: <LinkElement src={MisLotes} text="Mis lotes" link="/lots-manage" />,
-    },
-    {
-      id: 2,
-      elementList: <LinkElement src={MiPerfil} text="Mi perfil" link="/my-profile" />,
-    },
-    /*{
-      id: 3,
-      elementList: (
-        <LinkElement
-          src={CrearTrabajador}
-          text="Crear un usuario trabajador"
-          link="/crear-trabajador"
-        />
-      ),
-    },
-    {
-      id: 4,
-      elementList: (
-        <LinkElement src={VerTrabajador} text="Ver mis trabajadores" link="/ver-trabajadores" />
-      ),
-    },*/
-    {
-      id: 5,
-      elementList: (
-        <button
-          style={{
-            border: "none",
-            background: "none",
-            cursor: "pointer",
-          }}
-          onClick={() => logout()}
-        >
-          <Item src={CerrarSesion} alt="Cerrar Sesion" />
-          <Text>Cerrar sesión</Text>
-        </button>
-      ),
-    },
-  ];
-
-  return (
-    <>
-      <Navbar />
-      <MainWrapper>
-        <ContentArea>
-          <Text>¡Bienvenido {userData.name}!</Text>
-          <Menu>
-            {MenuItems.map((item, index) => (
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                key={index}
-              >
-                {item.elementList}
-              </div>
-            ))}
-          </Menu>
-        </ContentArea>
-      </MainWrapper>
-    </>
-  );
-};
 
 const LinkElement: React.FC<LinkElementProps> = ({ src, text, link }) => (
   <Link to={link} style={{ textDecoration: "none", color: "inherit" }}>
     <ItemsMenu>
       <Item src={src} alt={text} />
+      <Text>{text}</Text>
     </ItemsMenu>
-    <Text>{text}</Text>
   </Link>
 );
+
+const MainMenu: React.FC = () => {
+  const { userData } = useContext(AppContext);
+
+  const menuItems = [
+    { id: 1, src: MisLotes, text: "Mis lotes", link: "/lots-manage" },
+    { id: 2, src: MiPerfil, text: "Mi perfil", link: "/my-profile" },
+  ];
+
+  return (
+    <>
+      <MainLayout>
+        <Text>¡Bienvenido {userData.name}!</Text>
+        <Description>
+          Wango es un software diseñado para el cultivo del mango colombiano, específicamente de la
+          variedad "Tommy Atkins". Esta herramienta es esencial para cuantificar la huella hídrica
+          en todas sus dimensiones y sugerir estrategias para su reducción. Al combinar tecnología y
+          agricultura, Wango promueve un enfoque integral que preserva los recursos y maximiza la
+          producción del mango.
+        </Description>
+        <Menu>
+          {menuItems.map(({ id, src, text, link }) => (
+            <LinkElement key={id} src={src} text={text} link={link} />
+          ))}
+        </Menu>
+      </MainLayout>
+    </>
+  );
+};
 
 export default MainMenu;
