efca82a7fc2478ed0c860a32740937aa55d40040
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-11-18 08:26:29 -0500

    refactor: replace RegisterForm with SignUp in Dashboard component; streamline conditional rendering

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,21 +1,14 @@
 import React, { useContext } from "react";
+import { LoadingAnimation } from "../components/Loading";
 import { AppContext } from "../context/AppContext";
 import MainMenu from "./MainMenu";
-import RegisterForm from "./RegisterForm";
-import { LoadingAnimation } from "../components/Loading";
+import { SignUp } from "./SignUp";
 
 export const Dashboard: React.FC = () => {
   const { userData, appContextIsFetching } = useContext(AppContext);
+
   if (appContextIsFetching) {
     return <LoadingAnimation />;
   }
-  return (
-    <>
-      {userData && userData.security.identity_verified ? (
-        <MainMenu />
-      ) : (
-        <RegisterForm></RegisterForm>
-      )}
-    </>
-  );
+  return <>{userData && userData.security.identity_verified ? <MainMenu /> : <SignUp></SignUp>}</>;
 };
