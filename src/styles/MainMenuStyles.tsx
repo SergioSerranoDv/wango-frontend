1fcb32d71506f724f46a4c5c60c45fbbc7c82478
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-11-18 09:26:26 -0500

    refactor: remove unused styles from MainMenuStyles and App.css; simplify layout structure

--- src/styles/MainMenuStyles.tsx
+++ src/styles/MainMenuStyles.tsx
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
-export const MainWrapper = styled.div`
-  background-color: #fff;
-  display: flex;
-  max-width: 720px;
-  max-height: 1280px;
-  width: 100%;
-  flex-direction: column;
-  margin: 0 auto;
-  padding: 26px 0 80px;
-`;
-
 export const Hero = styled.div`
   display: flex;
   align-items: center;
@@ -29,38 +18,6 @@ export const Text = styled.div`
   font-size: 14px;
   text-align: center;
   color: black;
-`;
-
-export const ContentArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 20px;
-  padding: 0 72px;
-`;
-
-export const Menu = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 20px;
-  margin-top: 25px;
-`;
-
-export const ItemsMenu = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 12px;
-`;
-
-export const Item = styled.img`
-  width: 76px;
-  max-width: 100%;
-  object-fit: contain;
-
-  &.small {
-    width: 50px;
-  }
 `;
 
 export const TopCardsContainer = styled.div`
