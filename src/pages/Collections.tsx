b5079132a2fb4ab4ef39a8fbdd5cc18f7dfce34a
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-11-20 07:56:08 -0500

    refactor: integrate UsePagination hook and format date in Collections component

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -4,9 +4,11 @@ import { CollectionActions } from "../components/actions/CollectionActions";
 import { TableV1 } from "../components/tables/TableV1";
 import { ApiContext } from "../context/ApiContext";
 import { UseGet } from "../hooks/UseGet";
+import { UsePagination } from "../hooks/UsePagination";
 import { Collection } from "../interfaces/collection";
 import { Crop } from "../interfaces/crop";
 import { MainLayout } from "../layouts/MainLayout";
+import { formatDate } from "../services/Date";
 import { findCropById } from "../services/crop_s";
 import { Text } from "../styles/MainMenuStyles";
 
@@ -14,8 +16,7 @@ export const Collections: React.FC = () => {
   const { id } = useParams();
   const cropId = id || "";
   const { backendApiCall, serviceIsReady } = useContext(ApiContext);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [rowsPerPage, setRowsPerPage] = useState<number>(5);
+  const { currentPage, setCurrentPage, rowsPerPage, setRowsPerPage } = UsePagination();
   const { data, loading, setRefetch } = UseGet({
     endpoint: `v1/collection/info/crop/${cropId}/paginated?page=${currentPage}&limit=${rowsPerPage}`,
   });
@@ -32,40 +33,26 @@ export const Collections: React.FC = () => {
     if (serviceIsReady) {
       fetchCropData();
     }
-  }, [cropId, serviceIsReady]);
-
-  const formattedCollections = data?.collections?.map(
-    (collection: { createdAt: string | number | Date; updatedAt: string | number | Date }) => ({
-      ...collection,
-      createdAt: new Date(collection.createdAt).toLocaleString(undefined, {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-      updatedAt: new Date(collection.updatedAt).toLocaleString(undefined, {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    })
-  );
+  }, [backendApiCall, cropId, serviceIsReady]);
 
   const columns = useCallback(
     () => [
       {
-        title: "Nombre",
-        dataIndex: "name",
-        render: (record: Collection) => <span>{record.name}</span>,
-      },
-      {
         title: "Fecha Inicio",
         dataIndex: "createdAt",
-        render: (record: Collection) => <span>{record.createdAt}</span>,
+        render: (record: Collection) => <span>{formatDate(record.createdAt)}</span>,
       },
       {
         title: "Fecha Fin",
         dataIndex: "_id",
-        render: (record: any) => <span>{record.updatedAt ? record.updatedAt : "En proceso"}</span>,
+        render: (record: any) => (
+          <span>{record.updatedAt ? formatDate(record.updatedAt) : "En proceso"}</span>
+        ),
+      },
+      {
+        title: "Nombre",
+        dataIndex: "name",
+        render: (record: Collection) => <span>{record.name}</span>,
       },
       {
         title: "Estado",
@@ -82,18 +69,18 @@ export const Collections: React.FC = () => {
         ),
       },
     ],
-    []
+    [setRefetch]
   );
 
   return (
     <MainLayout>
       <Text style={{ marginBottom: "2rem" }}>Recolecciones del cultivo '{cropData.name}'</Text>
-      {!loading && formattedCollections && (
+      {!loading && (
         <TableV1
           evencolor="#FFFFFF"
           oddcolor="rgb(255, 103, 15, 0.2)"
           columns={columns()}
-          data={formattedCollections}
+          data={data.collections || []}
           pagination={{
             currentPage,
             setCurrentPage,
