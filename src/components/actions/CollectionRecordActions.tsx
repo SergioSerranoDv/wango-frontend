ad0c7375495e9077037530412e5db72f977650bb
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-11-25 11:22:40 -0500

    feat: implement context menu for collection record actions with Material-UI

--- src/components/actions/CollectionRecordActions.tsx
+++ src/components/actions/CollectionRecordActions.tsx
@@ -1,17 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ApiContext } from "../../context/ApiContext";
 import { Edit } from "../../icons/Actions";
-import { MoreOptions } from "../../icons/MoreOptions";
-import { Dropdown } from "../Dropdown";
+import { MoreVert } from "@mui/icons-material";
+import { Menu, Typography, Tooltip, IconButton, MenuItem } from "@mui/material";
 
 export const CollectionRecordActions = () => {
   const { backendApiCall } = useContext(ApiContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = React.useState(false);
+  const elRef = useRef<HTMLButtonElement>(null);
 
-  const actions = [
+  const menuItems = [
     {
       action: () => setIsEditModalOpen(true),
       icon: <Edit />,
@@ -26,15 +27,27 @@ export const CollectionRecordActions = () => {
   ];
   return (
     <div>
-      <MoreOptions
-        style={{ pointerEvents: isDropdownOpen ? "none" : "auto" }}
-        isDropdownOpen={isDropdownOpen}
-        setIsDropdownOpen={setIsDropdownOpen}
-      />
+      <Tooltip title="Opciones" placement="top">
+        <IconButton ref={elRef} onClick={() => setIsMenuOpen(true)}>
+          <MoreVert />
+        </IconButton>
+      </Tooltip>
 
-      {isDropdownOpen && (
-        <Dropdown closeDropdown={() => setIsDropdownOpen(false)} items={actions} />
-      )}
+      <Menu open={isMenuOpen} anchorEl={elRef.current} onClose={() => setIsMenuOpen(false)}>
+        {menuItems.map((item, index) => (
+          <MenuItem
+            color="#4c443f"
+            key={index}
+            onClick={() => {
+              setIsMenuOpen(false);
+              item.action();
+            }}
+          >
+            <span style={{ marginRight: "4px" }}>{item.icon}</span>
+            <Typography fontSize={12}>{item.name}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
     </div>
   );
 };
